2f113a7958e9e0640232e5c71d87c7b6fb1d0935
MiguelDiaz02 <g5diazcamposmiguel@gmail.com>
2025-03-30 00:40:25 +0000

    essential files

---
+++ cellRanger_protocol/absolutRoots_AML_POST.sh
@@ -0,0 +1,4 @@
+cellranger count --id= AML-POST-TRANSPLANT\
+   --fastqs=/home/mdiaz/hackaton-csbig/cellRanger_protocol/samples/AML_post_transplant \
+   --sample=read \
+   --transcriptome= /home/mdiaz/hackaton-csbig/cellRanger_protocol/refdata-gex-GRCh38-2024-A --create-bam true
---
+++ cellRanger_protocol/run_AML-PRE_cellranger.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Configuración de rutas relativas
+SCRIPT_DIR=$(dirname "$0")
+FASTQ_DIR="$SCRIPT_DIR/samples/fastqs/AML_pre_transplant"
+TRANSCRIPTOME_DIR="$SCRIPT_DIR/refdata-gex-GRCh38-2024-A"
+OUTPUT_ID="AML-PRE-TRANSPLANT"
+
+# Verificación de la existencia de directorios
+if [ ! -d "$FASTQ_DIR" ]; then
+  echo "Error: El directorio de FASTQ '$FASTQ_DIR' no existe."
+  exit 1
+fi
+
+if [ ! -d "$TRANSCRIPTOME_DIR" ]; then
+  echo "Error: El directorio del transcriptoma '$TRANSCRIPTOME_DIR' no existe."
+  exit 1
+fi
+
+# Ejecución de cellranger count
+cellranger count \
+  --id="$OUTPUT_ID" \
+  --fastqs="$FASTQ_DIR" \
+  --sample=read \
+  --transcriptome="$TRANSCRIPTOME_DIR" \
+  --create-bam=true
+
+echo "Procesamiento de cellranger completado."
