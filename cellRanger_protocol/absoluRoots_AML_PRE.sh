ece87bb9b5192fec29ea3402b5d6f81d6fdc2054
MiguelDiaz02 <g5diazcamposmiguel@gmail.com>
2025-03-30 01:39:22 +0000

    deleting unusefull scripts

--- cellRanger_protocol/absoluRoots_AML_PRE.sh
+++
@@ -1,4 +0,0 @@
-cellranger count --id=leukimia \
-   --fastqs=/home/mdiaz/hackaton-csbig/ranger/piloto/fastqs/flowcell1 \
-   --sample=read \
-   --transcriptome=/home/mdiaz/hackaton-csbig/ranger/piloto/apps/refdata-gex-GRCh38-2024-A --create-bam true
